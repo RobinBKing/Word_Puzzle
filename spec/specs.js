552d0c185bd8210e899ec3aca49b193a3129b708
Lindsey Raffa and Rob King <lindsey.raffa@gmail.com>
2016-01-21 09:38:35 -0800

    input second test and passed the spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe('vowelCheck', function() {
-  it("is true for any word with a vowel", function() {
+  it("is true for any sentence with a vowel", function() {
       expect(vowelCheck("check for vowels")).to.equal(true);
+  });
+  it("is false for any sentence with no vowels", function() {
+      expect(vowelCheck("lr rk")).to.equal(false);
   });
 });
